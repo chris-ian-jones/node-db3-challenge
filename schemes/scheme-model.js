941d393e14cb67f0c4166c78839372ab5f03984b
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-11 21:29:04 +0100

    WIP: setup helper method for findById(id)

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,15 @@ const db = require('./../data/db-config')
 
 module.exports = {
   find,
+  findById,
 }
 
 function find() {
   return db('schemes')
+}
+
+function findById(id) {
+  return db('schemes as s')
+    .where('s.id', id)
+    .first()
 }
